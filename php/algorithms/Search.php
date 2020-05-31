1617fd2e5a49e3bb3a0c6e63cb335ac502b46a30
Edward Rees <erees.hk@gmail.com>
2020-06-01 03:42:07 +0800

    documentation

--- php/algorithms/Search.php
+++ php/algorithms/Search.php
@@ -1,35 +1,54 @@
 <?php
-class Search {
+class Search
+{
   
-  /** Static function for binary search
+  /**
+   * Static function for binary search
+   *
+   * Run time: o(log(n))
+   *
    * @param $collection an iterable comparable collection of values
    * @param $value value to be searched for
    * @param $min minimum index - default to 0
    * @param $max maximum index - default to null
+   *
+   * @return int index of the value, -1 if not found
    */
-  public static function binarySearch($collection, $value, $min = 0, $max = null){
-    if($max == null){
-      $max = sizeof($collection);
+    public static function binarySearch($collection, $value, $min = 0, $max = null)
+    {
+        if ($max == null) {
+            $max = sizeof($collection);
+        }
+        if ($min >= $max) {
+            return -1;
+        }
+        $mid = (int)(($min + $max) / 2);
+        if ($collection[$mid] == $value) {
+            return $mid;
+        } elseif ($collection[$mid] > $value) {
+            return Search::binarySearch($collection, $value, $min, $mid);
+        } else {
+            return Search::binarySearch($collection, $value, $mid + 1, $max);
+        }
     }
-    if($min >= $max){
-      return -1;
-    }
-    $mid = (int)(($min + $max) / 2);
-    if($collection[$mid] == $value){
-      return $mid;
-    } else if($collection[$mid] > $value){
-      return Search::binarySearch($collection, $value, $min, $mid);
-    } else {
-      return Search::binarySearch($collection, $value, $mid + 1, $max);
-    }
-  }
 
-  public static function sequentialSearch($collection, $value){
-    for($i = 0; $i < sizeof($collection); $i++){
-      if($collection[$i] == $value){
-        return $i;
-      }
+    /**
+     * Sequential search function
+     *
+     * Run time: o(n)
+     *
+     * @param $collection iterable comparable collection to search
+     * @param $value specific value to be searched for
+     *
+     * @return int index of value if found, -1 if not
+     */
+    public static function sequentialSearch($collection, $value)
+    {
+        for ($i = 0; $i < sizeof($collection); $i++) {
+            if ($collection[$i] == $value) {
+                return $i;
+            }
+        }
+        return -1;
     }
-    return -1;
-  }
 }
