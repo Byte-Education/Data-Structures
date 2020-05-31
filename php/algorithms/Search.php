01ee749edcff9f0155d1709f47a2cf818bd06c6b
Edward Rees <erees.hk@gmail.com>
2020-06-01 03:37:53 +0800

    Addition of binary and sequential searches

--- php/algorithms/Search.php
+++ php/algorithms/Search.php
@@ -1,6 +1,12 @@
 <?php
 class Search {
   
+  /** Static function for binary search
+   * @param $collection an iterable comparable collection of values
+   * @param $value value to be searched for
+   * @param $min minimum index - default to 0
+   * @param $max maximum index - default to null
+   */
   public static function binarySearch($collection, $value, $min = 0, $max = null){
     if($max == null){
       $max = sizeof($collection);
@@ -11,19 +17,19 @@ class Search {
     $mid = (int)(($min + $max) / 2);
     if($collection[$mid] == $value){
       return $mid;
-    } else if($collection[$mid] > $collection[$min]){
-      return Search::binarySearch($collection, $value, $mid + 1, $max);
-    } else {
+    } else if($collection[$mid] > $value){
       return Search::binarySearch($collection, $value, $min, $mid);
+    } else {
+      return Search::binarySearch($collection, $value, $mid + 1, $max);
     }
   }
+
+  public static function sequentialSearch($collection, $value){
+    for($i = 0; $i < sizeof($collection); $i++){
+      if($collection[$i] == $value){
+        return $i;
+      }
+    }
+    return -1;
+  }
 }
-
-
-$collection = array ();
-$collection[] = 1;
-$collection[] = 5;
-$collection[] = 8;
-$collection[] = 10;
-$collection[] = 12;
-echo Search::binarySearch($collection, 1);
