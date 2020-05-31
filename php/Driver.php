5de056182e95d5b7c69fdecbe9003e7acae83e2e
Edward Rees <erees.hk@gmail.com>
2020-05-31 16:10:15 +0800

    change of php test driver

--- php/Driver.php
+++ php/Driver.php
@@ -6,57 +6,60 @@ include "structs/Queue.php";
 include "structs/PriorityQueue.php";
 
 
-function testQueue()
-{
+class TestQueue{
+  public static function main(){
     $queue = new Queue();
     $queue -> enqueue("Hello");
     $queue -> enqueue("World");
-  
+    
     echo "Enqueued: ";
     $queue -> printData();
     echo "\n";
-  
+    
     echo "Dequeued: ";
     echo $queue -> dequeue();
     echo "\n";
-  
+    
     echo "Queue: ";
     $queue -> printData();
     echo "\n";
+  }
 }
 
-function testSet()
-{
-  $set = new Set();
-  $set -> add("Hello");
-  $set -> add("Hello");
-  $set -> add("Bye");
-
-  echo $set -> size();
+class TestSet{
+  public static function main(){
+    $set = new Set();
+    $set -> add("Hello");
+    $set -> add("Hello");
+    $set -> add("Bye");
+    
+    echo $set -> size();
+  }
 }
 
-// testSet();
+class TestPriorityQueue{
+  public static function main(){
+    $set = new Set();
+    $set -> add("Hello");
+    $set -> add("World");
+  
+    $p_queue = new PriorityQueue();
+    $p_queue -> insert("hello");
+    $p_queue -> insert("john", 20);
+    $p_queue -> insert("world");
+    $p_queue -> insert("edward", 100);
+    $p_queue -> insert($set);
+    echo "Data:\n";
+    $p_queue -> printData();
+    echo "\n";
+    echo "Deleted: \n";
+    echo $p_queue -> delete(true);
+    echo "\n\n";
+    echo "Data:\n";
+    $p_queue -> printData();
+    // echo "\n";
+  }
 
-function testPriorityQueue(){
-  $set = new Set();
-  $set -> add("Hello");
-  $set -> add("World");
-
-  $p_queue = new PriorityQueue();
-  $p_queue -> insert("hello");
-  $p_queue -> insert("john", 20);
-  $p_queue -> insert("world");
-  $p_queue -> insert("edward", 100);
-  $p_queue -> insert($set);
-  echo "Data:\n";
-  $p_queue -> printData();
-  echo "\n";
-  echo "Deleted: \n";
-  echo $p_queue -> delete(true);
-  echo "\n\n";
-  echo "Data:\n";
-  $p_queue -> printData();
-  // echo "\n";
 }
 
-testPriorityQueue();
+TestPriorityQueue::main();
